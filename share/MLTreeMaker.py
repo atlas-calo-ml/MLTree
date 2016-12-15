e2ce9de9d9d52eb2b84c69a34912cb1530c9906a
Joakim Olsson <j.mr.olsson@gmail.com>
2016-12-14 17:16:00 -0800

    Minor fix

--- share/MLTreeMaker.py
+++ share/MLTreeMaker.py
@@ -5,15 +5,18 @@
 # @date     October 2016
 #
 
-theApp.EvtMax = -1
-import AthenaPoolCnvSvc.ReadAthenaPool
-
 ## MC15c pi+ test
 # svcMgr.EventSelector.InputCollections = [ "/afs/cern.ch/user/j/jolsson/work/datasets/mc15_13TeV.428001.ParticleGun_single_piplus_logE0p2to2000.recon.ESD.e3501_s2832_r8014/ESD.08446309._000227.pool.root.1" ]
 ## MC15a pi+ test
 # svcMgr.EventSelector.InputCollections = ["/afs/cern.ch/user/j/jolsson/work/datasets/mc15_13TeV.428001.ParticleGun_single_piplus_logE0p2to2000.recon.ESD.e3501_s2141_s2132_r6569/ESD.05281497._001502.pool.root.1" ]
 ## MC15a pi0 test
 svcMgr.EventSelector.InputCollections = [ "/afs/cern.ch/user/j/jolsson/work/datasets/mc15_13TeV.428000.ParticleGun_single_pi0_logE0p2to2000.recon.ESD.e3496_s2139_s2132_r6569/ESD.05281500._001571.pool.root.1" ]
+
+theApp.EvtMax = -1
+import AthenaPoolCnvSvc.ReadAthenaPool
+
+from AthenaCommon.GlobalFlags import jobproperties
+jobproperties.Global.DetDescrVersion='ATLAS-R2-2015-04-00-00'
 
 # Setup MLTreeMaker algorithm
 from AthenaCommon import CfgMgr
@@ -45,8 +48,7 @@ from RecExConfig.ObjKeyStore import ObjKeyStore, objKeyStore
 oks = ObjKeyStore()
 oks.addStreamESD('CaloCellContainer', ['AllCalo'] )
 
-## Suggestion from Peter Loch to turn off local cluster calibration, at the
-## moment this makes Athena crash
+## Suggestion from Peter Loch to turn off local cluster calibration, at the moment this makes Athena crash
 # from CaloRec.CaloTopoClusterFlags import jobproperties
 # jobproperties.CaloTopoClusterFlags.doTopoClusterLocalCalib.set_Value_and_Lock(False)
 # from CaloRec.CaloClusterTopoGetter import CaloClusterTopoGetter
