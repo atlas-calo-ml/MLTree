571d10e687d3ed6675098449876f38cda64e9824
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2023-05-24 16:53:02 +0200

    Update dijets config to output jets to the ntuple.

--- python/runDiJets.py
+++ python/runDiJets.py
@@ -9,7 +9,7 @@ if __name__=="__main__":
     from MLTree.MainCfg import __MLTree
     cfgFlags.addFlagsCategory("MLTree",__MLTree)
 
-    cfgFlags.Exec.MaxEvents=1000
+    cfgFlags.Exec.MaxEvents=-1
     cfgFlags.Input.isMC=True
     cfgFlags.Input.Files= ["/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/PFlowTests/mc16_13TeV/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.recon.ESD.e6337_e5984_s3170_r12674/ESD.25732025._000034.pool.root.1"]
     cfgFlags.Concurrency.NumThreads=1
@@ -41,8 +41,8 @@ if __name__=="__main__":
                            TruthParticles = True,
                            EventTruth = False,
                            G4TruthParticles = False,
-                           Jets = False,
-                           JetContainers = ["AntiKt4EMTopoJets","AntiKt4LCTopoJets","AntiKt4TruthJets"],
+                           Jets = True,
+                           JetContainers = ["AntiKt4EMPFlowJets","AntiKt4TruthJets"],
                            RootStreamName = "OutputStream"))                         
 
     cfg.run()
